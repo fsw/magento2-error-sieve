a4186da104e618262c33b42c4ad27005e2019c05
Franciszek Wawrzak <f.wawrzak@macopedia.com>
2020-09-21 21:56:20 +0200

    graph ql error handling fix

---
+++ GraphQl/ErrorHandler.php
@@ -0,0 +1,24 @@
+<?php
+namespace Fsw\ErrorSieve\GraphQl;
+
+use Fsw\ErrorSieve\Model\ExceptionSieve;
+use Magento\Framework\GraphQl\Query\ErrorHandlerInterface;
+
+class ErrorHandler implements ErrorHandlerInterface
+{
+    /**
+     * Handle errors
+     *
+     * @param \GraphQL\Error\Error $errors
+     * @param callable $formatter
+     *
+     * @return array
+     */
+    public function handle(array $errors, callable $formatter): array
+    {
+        foreach ($errors as $e) {
+            ExceptionSieve::saveException($e);
+        }
+        return array_map($formatter, $errors);
+    }
+}
